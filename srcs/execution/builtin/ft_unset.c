52cc9408b4318d65056ff704a051987ab6ecc84d
Elmostafa Khartiti <elkharti@e1r2p12.1337.ma>
2025-05-06 09:43:30 +0100

    start unset

--- srcs/execution/builtin/ft_unset.c
+++ srcs/execution/builtin/ft_unset.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-static bool exist(char **args)
+static bool exist(char **args, char *key)
 {
 
 }
@@ -14,5 +14,13 @@ void    ft_unset(t_data *data, char **args)
         return ;
     }    
     i = 1;
-    lkjgalkjg
+    while (args[i])
+    {
+        if(!is_valid_key(args[i]))
+        {
+            printf("unset: `%s': not a valid identifier", args[i]);
+            data->exit_status = 1;
+            exit(1);
+        }
+    }
 }
